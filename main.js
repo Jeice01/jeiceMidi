cb4768a4b4b5c77f253b52c66d36c936333264a3
Jeice Santos <jeice.santos@gmail.com>
2023-11-17 10:16:57 -0300

    subindo o projeto para o GitHub

---
+++ main.js
@@ -0,0 +1,34 @@
+function tocaSom(seletorAudio){
+   const elemento =  document.querySelector(seletorAudio);
+   if(elemento && elemento.localName === 'audio'){
+       elemento.play();
+   }else{
+        //alert('Elemento não encontrado ou seletor inválido');
+        console.log('Elemento não encontrado ou seletor inválido');
+   }
+}
+
+const listaDeTeclas =  document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+    
+};
